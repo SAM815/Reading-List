4793717d41d9212fbd21bbc5070e3c28ed426a5a
Samman <sammanbhetwal2025@gmail.com>
2023-03-28 12:14:52 -0400

    first commit

---
+++ src/components/NewBookForm.js
@@ -0,0 +1,30 @@
+//NewBookForm.js
+import React, { useContext, useState } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import { bookReducer } from '../reducers/bookReducer';
+
+const NewBookForm = () => {
+  const { dispatch } = useContext(BookContext);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(title, author);
+   dispatch({type: 'ADD_BOOK', book: {title, author}})
+    setTitle('');
+    setAuthor('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="book title" value={title}
+        onChange={(e) => setTitle(e.target.value)} />
+      <input type="text" placeholder="author name" value={author}
+        onChange={(e) => setAuthor(e.target.value)} />
+      <input type="submit" value="add book" />
+    </form>
+  );
+}
+ 
+export default NewBookForm;
